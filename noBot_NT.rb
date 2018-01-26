7079d813dd59ad0b254d6ab9b2b32f7a77edc358
Atterack833 <35739574+Atterack833@users.noreply.github.com>
2018-01-25 22:59:25 -0500

    bot that says no

--- noBot_NT.rb
+++ noBot_NT.rb
@@ -3,35 +3,6 @@ require 'discordrb'
 $bot = Discordrb::Bot.new token: '###########################################################'
 
 
-# Non-passive commands (You have to @mention) the bot
-$bot.mention() do | event|
-		command ="#{ event.message.content}"
-		channel = event.message.channel
-		user = event.message.user
-		commands( command, channel, user ) 
-end
-
-#TODO Add more commands
-def commands( command, channel, user )
-	case command	
-		when /How many servers are you on/
-			message = "I'm on as many are on my Devs terminal page Baka (Does he think I'd show him that info)"
-			puts $bot.servers
-		when /I love you/
-			message = "Kill yourself"
-                when /i love you/
-			message = "Kill yourself and learn how to capitalize"
-		when /Why are you so mean/
-			message	= "Because you're an idiot " + user.display_name
-		when /How are you/
-			message = "Why would I tell you how I am Baka " + user.display_name
-		else
-			message = "What the hell are you going on about Baka " + user.display_name
-	end
-	channel.send_message( message )
-end
-
-
 #Passive commands
 $bot.message() do| event|
 		message = event.message.content
